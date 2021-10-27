3a676741d51dec095997797a640b02a054619fba
Dylan <dylanthomasshine@gmail.com>
2021-10-27 01:01:04 +0000

    editing Readme file and adding text to files

--- order/forms.py
+++ order/forms.py
@@ -6,7 +6,7 @@ from .widgets import CustomClearableFileInput
 class OrderForm(forms.ModelForm):
     name = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Full name'}))
     image_url = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-    description = forms.CharField(label="", widget=forms.Textarea(attrs={'placeholder': 'Description'}))
+    description = forms.CharField(label="", widget=forms.Textarea(attrs={'placeholder': 'Please tell us what you would like us to create for you, then simply upload an image for a reference. '}))
     
 
 # added 'form-control' for responsiveness
