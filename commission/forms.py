16c59052f3edb54633a8ec1a2e8a366a8c06e395
Dylan <dylanthomasshine@gmail.com>
2021-11-05 10:58:55 +0000

    rendered commission form to show a message for user

--- commission/forms.py
+++ commission/forms.py
@@ -4,10 +4,10 @@ from .models import Commission
 
 
 class CommissionForm(forms.ModelForm):
-
-    class Meta:
-        model = Commission
-        fields = '__all__'
+    first_name = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'First name'}))
+    last_name = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
+    email = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder': 'Email address'}))
+    message = forms.CharField(label="", widget=forms.Textarea(attrs={'placeholder': 'Please give a description of what you would like created and then upload your image below'}))
 
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 # added 'form-control' for responsiveness
@@ -17,3 +17,6 @@ class CommissionForm(forms.ModelForm):
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
 
+    class Meta:
+        model = Commission
+        fields = ('first_name', 'last_name', 'email', 'message')
